65876369863c24fd62ed8b4dce74c3665d1997c3
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2022-07-23 21:56:31 +0100

    Delete New folder/navigtion directory

--- New folder/navigtion/main header/MainHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./main-header.scss"
-
-const MainHeader: React.FC<any> = ({ children }) => {
-  return <header className="main-header">{children}</header>;
-};
-
-export default MainHeader;
-
